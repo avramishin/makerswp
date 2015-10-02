16ebda91e4802e0d8bab27b97988f6c4f87a298f
Nikolay Neizvesny <zobaken@gmail.com>
2015-10-02 17:24:29 +0300

    blog

---
+++ wp-content/themes/makers/single.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Template Name: Blog Post
+ * @package WordPress
+ */
+get_header(); ?>
+    <main>
+
+        <div class="hero">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/hero.jpg" alt="">
+
+            <div class="overlay"></div>
+            <div class="texture"></div>
+            <div class="hero-inner">
+                <h2>Our community is our strength.</h2>
+
+                <p>Makers innovates through collaboration and hard work.</p>
+            </div>
+        </div>
+
+        <div class="sub-navigation">
+            <div class="sub-navigation-inner">
+                <div class="sub-nav-newsletter">
+                    <label for="email">Don't miss our news</label>
+                    <input type="email" id="email" placeholder="E-mail address in here">
+                    <input type="submit" value="Sign up">
+                </div>
+            </div>
+        </div>
+
+        <div class="container">
+            <article>
+                <?php while (have_posts()) : ?>
+                    <header class="center">
+                        <h2><?php the_title() ?></h2>
+                    </header>
+                    <section class="content">
+                        <?php
+                        the_post();
+                        ?>
+                    </section>
+                <?php endwhile; ?>
+
+            </article>
+        </div>
+
+    </main>
+<?php get_footer(); ?>
