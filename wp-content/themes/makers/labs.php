3a47dd9a1f743f7bde7b67253210845704b42ece
Isaac <isaac@Isaacs-MacBook-Pro.local>
2015-10-06 14:41:20 +0200

    text changes

--- wp-content/themes/makers/labs.php
+++ wp-content/themes/makers/labs.php
@@ -11,8 +11,8 @@ get_header(); ?>
     <div class="overlay"></div>
     <div class="texture"></div>
     <div class="hero-inner">
-      <h2>The greater the team, the… bla, bla!</h2>
-      <p>Team Spirit at it’s best!</p>
+      <h2>Our community is our strength.</h2>
+      <p>Makers innovates through collaboration and hard work.</p>
     </div>
   </div>
 
@@ -33,12 +33,18 @@ get_header(); ?>
 
   <div id="identify" class="grid process">
     <div class="process-identify grid-item-wide">
-      <h3>1. Identify [idɛntifiˈʦiːʀən]</h3>
+      <h3>1. Identify</h3>
       <div class="process-content">
         <h4>Explenation of the word like in some kind of dictionary</h4>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship .Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
+        <p>
+          Did you know that most startups fail during the first six months? The main obstacles here include ineffecient teams, a lack of resources or failure to identify product-market fit. While success/failure of startups is oftentimes beyond our control, we believe the chances of failure should not be a stumbling block that constrains an entrepreneur’s imagination. Instead, we see it as a stepping stone to success.
+          <br>
+          <br>
+          So we built Labs, a lean startup labaratory where ideas materialise through iteration, testing and experimentation. We seek to minimize the financial risks, while maximizing efficiency by cutting down on the feedback cycles. Labs plays a crucial part of Makers by constantly providing new ideas and insights into different markets, as well as connecting entrepreneurs to the company.
+          <br>
+          <br>
+          One of the key questions we ask ourselves is “how can startups minimize the risks of failure?”. So we scrutinize the process of identifying a product/market fit. Once given a go-ahead, however, we execute fearlessly with an iterative, product-oriented approach and stride towards shipping minimum viable products.
+        </p>
       </div>
     </div>
     <div class="grid-item-wide">
@@ -50,43 +56,20 @@ get_header(); ?>
       <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/square-2.jpg" alt="" />
     </div>
     <div class="process-test grid-item-wide">
-      <h3>2. Test [idɛntifiˈʦiːʀən]</h3>
+      <h3>2. Test</h3>
       <div class="process-content">
         <h4>Explenation of the word like in some kind of dictionary</h4>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship .Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking.</p>
+        <p>
+          An idea that sounds innovative and convincing in theory may not necessarily work in practice. That is why we believe they should always be complemented by analytics and numbers, although we are advocates of entrepreneurs’ instincts.
+          <br>
+          <br>
+          Our team at Labs consists of product managers, developers, designers as well as marketing managers who are specialists in developing and shipping MVPs. MVPs allow us to test market acceptance for unfledged business ideas, as well as helping us identify room for improvement.
+          <br>
+          <br>
+          With Labs, we are not only able to reduce the risks of investments, but also gain tons of valuable insights through research during the process of development. The early iteration and lean company-building practices definitely set Makers apart others, and enables us to scientifically measure what others can only guess at.
+        </p>
       </div>
     </div>
   </div>
-  <div id="build" class="grid process">
-    <div class="process-build grid-item-wide">
-      <h3>3. Build [idɛntifiˈʦiːʀən]</h3>
-      <div class="process-content">
-        <h4>Explenation of the word like in some kind of dictionary</h4>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship .Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-      </div>
-    </div>
-    <div class="grid-item-wide">
-      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/square-3.jpg" alt="" />
-    </div>
-  </div>
-  <div id="scale" class="grid process">
-    <div class="grid-item-wide">
-      <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/square-4.jpg" alt="" />
-    </div>
-    <div class="process-scale grid-item-wide">
-      <h3>4. Scale [idɛntifiˈʦiːʀən]</h3>
-      <div class="process-content">
-        <h4>Explenation of the word like in some kind of dictionary</h4>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking to deliver user-first digital solutions on a worldwide scale.</p>
-        <p>Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship .Specialising in online customer experiences &amp; brand development, we combine digital craftsmanship with innovative thinking.</p>
-      </div>
-    </div>
-  </div>
-
 </main>
 <?php get_footer(); ?>
