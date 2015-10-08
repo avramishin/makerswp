338e5fa9d62082ed0d1fe747c96594acd6c81190
Vadim L. Avramishin <vavramishin@brightgrove.com>
2015-10-08 16:26:24 +0300

    fixes

--- wp-content/themes/makers/page-join-us-founder.php
+++ wp-content/themes/makers/page-join-us-founder.php
@@ -31,37 +31,88 @@ get_header(); ?>
         </div>
     </div>
 
-    <div class="container">
-        <article>
-            <section class="content">
-                <h4>Be smart</h4>
+        <section class="content">
+            <article class="article">
+                <header>
+                    <h1>We are looking for outstanding entrepreneurial talents</h1>
+                    <p class="subline">That share our vision to build next generation tech companies.</p>
+                </header>
+                <p class="intro">A startup's success is not only based on a great. market-ready product. but also on a complimentary and experienced team. Therefore we are always looking for entrepreneurial talents to join the MAKERS' team. These are our requirements a potential co-founder should meet:</p>
+            </article>
+        </section>
 
-                <h2>Develop your ideas and shape the future</h2>
-
-                <p>MAKERS offers an incredible package of technological expertise, all-round support and great terms all
-                    in exchange for your presence and open mind. We know that ideas gain bits and pieces as they bounce,
-                    and we cannot wait to hear about your exciting new project! Apply today and start acting on your
-                    ideas.</p>
-            </section>
-        </article>
-    </div>
-
-    <div class="bottom-quote">
-        <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/bottom-quote-benjamin-libor.jpg" alt="">
-
-        <div class="bottom-quote-text">
-            <div class="container right">
-                <h2>
-                    <span>„“</span><br/>
-                    Let's join our forces to build<br/>products that people love. </h2>
-
-                <p>
-                    Benjamin Libor<br/>
-                    Founder Labs </p>
+        <section class="boxes">
+            <div class="container">
+                <ul class="">
+                    <div class="boxes-row">
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-cloud.png" alt="icon-cloud"> Entrepreneurial minded people</li>
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-chemistry.png" alt="icon-chemistry"> Out-of-the-box thinkers</li>
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-thumbsup.png" alt="icon-thumbsup"> Hands-on doers</li>
+                    </div>
+                    <div class="boxes-row">
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-lightbulb.png" alt="icon-lightbulb"> lnspiring leaders</li>
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-star.png" alt="icon-star"> People who never liked making coffee </li>
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-star.png" alt="icon-star"> Pioneers who think big </li>
+                    </div>
+                </ul>
             </div>
+        </section>
+
+        <section class="quote">
+            <div class="quote-media" style="background-image: url(<?php echo esc_url(get_template_directory_uri()); ?>/images/founder-1.jpg);">
+                <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/founder-1.jpg" alt="Founder 1">
+            </div>
+            <div class="quote-content">
+                <blockquote>
+                    <p>MAKERS paved our way to success by actively supporting us to become Germany's robo-advisor No. 1 within less than one year.</p>
+                    <cite>Robert Henker, <span>CEO/Founder</span></cite>
+                </blockquote>
+            </div>
+        </section>
+
+        <section class="content">
+            <article class="article">
+                <header>
+                    <h1>We are looking for outstanding entrepreneurial talents</h1>
+                    <span class="subline">That share our vision to build next generation tech companies.</span>
+                </header>
+                <p class="intro">A startup's success is not only based on a great. market-ready product. but also on a complimentary and experienced team. Therefore we are always looking for entrepreneurial talents to join the MAKERS' team. These are our requirements a potential co-founder should meet:</p>
+            </article>
+        </section>
+
+        <section class="boxes">
+            <div class="container">
+                <ul class="">
+                    <div class="boxes-row">
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-cloud.png" alt="icon-cloud"> Entrepreneurial minded people</li>
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-chemistry.png" alt="icon-chemistry"> Out-of-the-box thinkers</li>
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-thumbsup.png" alt="icon-thumbsup"> Hands-on doers</li>
+                    </div>
+                    <div class="boxes-row">
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-lightbulb.png" alt="icon-lightbulb"> lnspiring leaders</li>
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-star.png" alt="icon-star"> People who never liked making coffee </li>
+                        <li><img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/icons/icon-star.png" alt="icon-star"> Pioneers who think big </li>
+                    </div>
+                </ul>
+            </div>
+        </section>
+
+        <div class="bottom-quote">
+            <img src="<?php echo esc_url(get_template_directory_uri()); ?>/images/bottom-quote-benjamin-libor.jpg" alt="">
+
+            <div class="bottom-quote-text">
+                <div class="container right">
+                    <h2>
+                        <span>„“</span><br>
+                        Let's join our forces to build<br>products that people love. </h2>
+
+                    <p>
+                        Benjamin Libor<br>
+                        Founder Labs </p>
+                </div>
+            </div>
+            <h3>Interested in the MAKERS Group. Benjamin Libor is your contact person.</h3>
         </div>
-        <h3>Interested in the MAKERS Group. Benjamin Libor is your contact person.</h3>
-    </div>
 
     </main>
 <?php get_footer(); ?>
