88ea24171ee87bd3c6ef46c6d485e245a23b6c5f
Vadim L. Avramishin <vavramishin@brightgrove.com>
2015-10-26 18:10:45 +0200

    imprint page

---
+++ wp-content/themes/makers/page-imprint.php
@@ -0,0 +1,121 @@
+<?php
+/**
+ * Template Name: Imprint
+ * @package WordPress
+ */
+
+get_header(); ?>
+<main>
+
+    <section class="container intro container-fluid">
+        <article class="article">
+            <header class="center blog">
+                <h1>IMPRINT</h1>
+            </header>
+        </article>
+
+
+        <div class="row row-fluid" style=" text-align:left;">
+            <div class="col-sm-6">
+                <div>
+                    <div>
+                        <h3 style="text-align: left;">Address</h3>
+                    </div>
+                    <div>
+                        <address>Vida Ventures GmbH<br>
+                            Invalidenstr. 112<br>
+                            10115 Berlin, Germany
+                        </address>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6">
+                <div>
+                    <h3 style="text-align: left;">Contact</h3>
+                    <address>
+                        <p><span class="s1">Tel.:<a href="tel://+493057703944">
+                                    +49 (0) 30 57703944<br>
+                                </a></span>Email: <a
+                                href="mailto:contact@makers.do">contact<span>@makers.do</span></a></p>
+                    </address>
+                </div>
+            </div>
+        </div>
+
+        <div class="row section row-fluid" style=" text-align:left;">
+            <div class="col-sm-12">
+                <div>
+                    <div style="height: 32px"><span></span></div>
+                    <div>
+                        <p>Managing Directors:<br>
+                            Friedrich A. Neuman, Marius Schulze</p>
+
+                        <p>Commercial Register:<br>
+                            Amtsgericht Charlottenburg; HRB 150840 B</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <div class="row  section row-fluid" style=" text-align:left;">
+            <div class="col-sm-12  ">
+                <div>
+                    <div style="height: 32px"><span></span></div>
+                    <div>
+                        <div>
+                            <h3>The following legal information is required according to
+                                German law:</h3>
+
+                            <p>Inhaltlich Verantwortlicher nach § 55 Abs.II RStV:
+                                Friedrich A. Neuman, Marius Schulze, Anschrift siehe oben. Hinweis
+                                zu externen Links: Mit Urteil vom <span
+                                    class="s1">12. Mai 1998</span>, “Haftung für Links” (312 O
+                                85/98) hat das Landgericht (LG) Hamburg entschieden, dass man durch
+                                die Anbringung eines Links die Inhalte der gelinkten Seite ggf. mit
+                                zu verantworten hat.</p>
+
+                            <p>Dies kann – so das LG – nur dadurch verhindert werden,
+                                dass man sich ausdrücklich von diesen Inhalten distanziert. Hiermit
+                                distanzieren wir uns ausdrücklich von allen Inhalten aller gelinkten
+                                Seiten auf unserer Website und erklären, dass wir uns diese Inhalte
+                                nicht zu Eigen machen.</p>
+
+                        </div>
+                    </div>
+                    <div style="height: 32px"><span></span></div>
+                    <div>
+                        <div>
+                            <h3>Haftungsausschluss</h3>
+
+                            <p>Die bereitgestellten Informationen auf dieser Website
+                                wurden sorgfältig geprüft und werden regelmäßig aktualisiert. Jedoch
+                                kann keine Garantie dafür übernommen werden, dass alle Angaben zu
+                                jeder Zeit vollständig, richtig und/oder in letzter Aktualität
+                                dargestellt sind. Dies gilt insbesondere für alle Links zu anderen
+                                Websites, auf die direkt oder indirekt verwiesen wird. Wir behalten
+                                uns vor alle Angaben ohne vorherige Ankündigung zu ergänzen, zu
+                                entfernen und/oder zu ändern.</p>
+
+                        </div>
+                    </div>
+                    <div style="height: 32px"><span></span></div>
+                    <div>
+                        <div>
+                            <h3>Copyright</h3>
+
+                            <p>Alle auf dieser Website genannten Produktnamen,
+                                Produktbezeichnungen, Markennahmen und Logos sind eingetragene
+                                Warenzeichen und/oder Eigentum der jeweiligen Rechteinhaber. Alle
+                                Texte und Grafiken dieser Website: © Vida Ventures GmbH oder die
+                                Rechteinhaber.</p>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
